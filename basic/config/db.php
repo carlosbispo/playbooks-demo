e07b48d138d86bc3a04c94b2c0e0bf24822c12c5
Carlos Bispo <a26633@gmail.com>
2017-06-26 20:26:34 +0100

    1st commit

---
+++ basic/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=testing_db',
+    'username' => 'vagrant',
+    'password' => 'vagrant',
+    'charset' => 'utf8',
+];
